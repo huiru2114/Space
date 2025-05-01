ef5ee188298b780e586b722d5c9838d22d3dd8f4
honey55-bool <adelinechanxyn5@gmail.com>
2025-05-02 02:22:20 +0800

    Added scrollable

--- app/src/main/java/com/example/space/AddTripActivity.java
+++ app/src/main/java/com/example/space/AddTripActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.provider.MediaStore;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -65,6 +66,8 @@ public class AddTripActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_trip);
+
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
 
         // Initialize SupabaseTrip
         supabaseTrip = new SupabaseTrip(this);
